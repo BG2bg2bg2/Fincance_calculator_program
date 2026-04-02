2f67d4e4e967d19b29443745a16af54dd69a3421
edwingsantos <edwing.santos@ucas-edu.net>
2026-04-01 22:00:37 -0600

    just to turn my stuff in

--- login_logout.py
+++ login_logout.py
@@ -5,21 +5,30 @@
 #make a function called login 
     
     #make a while true loop 
+        #if choice is to log in    
+            #make username match to an input asking for the username
 
-        #make username match to an input asking for the username
+            #open the filepath and read it as file 
+                #make csv_reader be a csv reader to the file
+                #header be next the csv reader 
+                #make rows be list csv reader
 
-        #open the filepath and read it as file 
-            #make csv_reader be a csv reader to the file
-            #header be next the csv reader 
-            #make rows be list csv reader
+            #make user found false
+            #make a loop in row 
+                #if there is not in row return 
+                #if there is a account make user found true
 
-        #make user found false
-        #make a loop in row 
-            #if there is not in row return 
-            #if there is a account make user found true
+            #call the funtion for gov to handle his part 
 
-    #ask user for what they want to obtain 
-    #make a while loop 
-        #make choice be an input
-        #if choice is 1 
+        #elif choice of the user create an account
+            #make a input asking the user for a name 
+            #ask the user if they want to type their own password 
+                #if choice is to type their own password, make them type the password
+                #elif choice is to generate a password call the random password generator 
+                #else print to select an acual option
+            #safe the password to the csv file 
+
+        #elif choice is to exit break and go to the main funtion 
+
+
 
